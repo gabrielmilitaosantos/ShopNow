93b0e5b6ba2086d54cb9a9b987545c71b5255ecc
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-05-06 17:42:30 -0300

    Look for assets, configure the libraries and create a navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import './Navbar.css';
+import Button from '@mui/material/Button';
+
+import logo from '../Assets/shopnow_logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+
+function Navbar() {
+    const [menu, setMenu] = useState("shop");
+
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="logo" width={65} height={65} />
+                <p>ShopNow</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => { setMenu("shop") }}>Shop {menu === "shop" && <hr />}</li>
+                <li onClick={() => { setMenu("men") }}>Men {menu === "men" && <hr />}</li>
+                <li onClick={() => { setMenu("women") }}>Women {menu === "women" && <hr />}</li>
+                <li onClick={() => { setMenu("kids") }}>Kids {menu === "kids" && <hr />}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <Button variant="outlined">Login</Button>
+                <img src={cart_icon} alt="cart icon" />
+                <div className="nav-cart-count">0</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
