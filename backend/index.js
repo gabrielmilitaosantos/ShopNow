363d3c6aff14e4675e44d726984d01ff961c1e7c
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-06-08 18:09:49 -0300

    Create Schema for new products, and endpoint for this as well

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,79 @@ app.get("/", (req, res) => {
     res.send("Express App is Running");
 });
 
+// Image Storage
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Upload Endpoint for images
+app.use('/images', express.static('upload/images'));
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+// Schema for Creating Products
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+app.post("/addproduct", async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        old_price: req.body.old_price,
+        new_price: req.body.new_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+    });
+});
+
 app.listen(port, (err) => {
     if (!err) {
         console.log("Server Running on Port: " + port);
