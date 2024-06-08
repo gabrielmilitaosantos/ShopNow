f7c602271988a400c35299ff3fe3f8d62af85026
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-06-08 17:12:29 -0300

    Configure database and start API

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,32 @@ import jwt from 'jsonwebtoken';
 import multer from 'multer';
 import path from 'path';
 import cors from 'cors';
+import env from 'dotenv';
 
 const port = 4000;
 const app = express();
+env.config();
 
 app.use(express.json());
 app.use(cors());
+
+// Database connection with MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+// API
+app.get("/", (req, res) => {
+    res.send("Express App is Running");
+});
+
+app.listen(port, (err) => {
+    if (!err) {
+        console.log("Server Running on Port: " + port);
+    }
+    else {
+        console.log("Error: " + err);
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Express working");
+});
