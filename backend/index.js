83ed1fc0648e755ac1c0b1c0683e6fdd430c3c52
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-07-19 17:44:12 -0300

    Create endpoint for login

--- backend/index.js
+++ backend/index.js
@@ -181,6 +181,30 @@ app.post("/signup", async (req, res) => {
     res.json({ success: true, token });
 });
 
+// Create endpoint for user login
+app.post("/login", async (req, res) => {
+    let user = await Users.findOne({ email: req.body.email });
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, process.env.JWT_SECRET);
+            res.json({ success: true, token });
+        }
+        else {
+            res.json({ success: false, errors: "Wrong Password" });
+        }
+    }
+    else {
+        res.json({ success: false, errors: "Invalid Email" });
+    }
+});
+
+
 app.listen(port, (err) => {
     if (!err) {
         console.log("Server Running on Port: " + port);
