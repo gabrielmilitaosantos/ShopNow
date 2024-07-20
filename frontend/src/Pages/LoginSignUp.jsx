6c3e6cf1ef4892cbc21ccf6373c0d2c4268918b4
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-07-20 15:15:43 -0300

    Connect data to inputs fields in Login/SignUp page (state)

--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '@mui/material/Button';
-
 import './CSS/LoginSignUp.css';
 
 function LoginSignUp() {
+
+    const [state, setState] = useState("Login");
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        email: ""
+    });
+
+    const changeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const login = async () => {
+        console.log("Função login", formData);
+    }
+
+    const signup = async () => {
+        console.log("Função signup", formData);
+    }
+
     return (
         <div className="login-signup">
             <div className="login-signup-container">
-                <h1>Sign Up</h1>
+                <h1>{state}</h1>
                 <div className="login-signup-fields">
-                    <input type="text" placeholder="Your Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
+                    {state === "Sign Up" && <input name="username" value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" />}
+                    <input name="email" value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" />
+                    <input name="password" value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
                 </div>
-                <Button variant="contained">Continue</Button>
-                <p className="signup-login">
-                    Already have a account? <span>Login here</span>
-                </p>
+                <Button variant="contained" onClick={() => { state === "Login" ? login() : signup() }}>Continue</Button>
+                {state === "Sign Up" ?
+                    <p className="signup-login">Already have a account? <span onClick={() => { setState("Login") }}>Login here</span></p>
+                    :
+                    <p className="signup-login">Create a account? <span onClick={() => { setState("Sign Up") }}>Click here</span></p>
+                }
                 <div className="login-signup-agree">
                     <input type="checkbox" name="" id="" />
                     <p>By continuing, i agree to the terms of use &
