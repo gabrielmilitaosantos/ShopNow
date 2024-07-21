f8a77d9352e67cafde6d15c0a6c4ad454290c660
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-07-21 15:54:32 -0300

    Create logic for login and signup sessions

--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -20,10 +20,44 @@ function LoginSignUp() {
 
     const login = async () => {
         console.log("Função login", formData);
+        let responseData;
+        await fetch("http://localhost:4000/login", {
+            method: "POST",
+            headers: {
+                Accept: 'application/form-data',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }).then((response) => response.json()).then((data) => responseData = data)
+
+        if (responseData.success) {
+            localStorage.setItem('auth-token', responseData.token);
+            window.location.replace("/");
+        }
+        else {
+            alert(responseData.errors);
+        }
     }
 
     const signup = async () => {
         console.log("Função signup", formData);
+        let responseData;
+        await fetch("http://localhost:4000/signup", {
+            method: "POST",
+            headers: {
+                Accept: 'application/form-data',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }).then((response) => response.json()).then((data) => responseData = data)
+
+        if (responseData.success) {
+            localStorage.setItem('auth-token', responseData.token);
+            window.location.replace("/");
+        }
+        else {
+            alert(responseData.errors);
+        }
     }
 
     return (
