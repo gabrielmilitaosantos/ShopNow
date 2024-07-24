e443865cd62b0d9ffc4df01364eac5c926fed389
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2024-07-24 16:31:32 -0300

    Replace data from static file with data provided by database in Popular & New Collections components

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Popular.css';
-import data_product from '../Assets/data';
 import Item from "../Item/Item";
 
 function Popular() {
+    
+    const [popularProducts, setPopularProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/popularinwomen")
+        .then((response) => response.json())
+        .then((data) => setPopularProducts(data))
+    }, []);
+
     return (
         <div className="popular">
             <h1>POPULAR IN WOMEN</h1>
             <hr />
             <div className="popular-item">
-                {data_product.map((item, index) => {
+                {popularProducts.map((item, index) => {
                     return (
                         <Item key={index}
                             id={item.id}
